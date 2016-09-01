71be91e89b7218d1bb613a9ed249912d8957e277
Alperen <alperentalaslioglu@users.noreply.github.com>
2016-09-01 09:42:42 +0300

    Update Queue.js

--- src/data-structures/Queue.js
+++ src/data-structures/Queue.js
@@ -1,16 +1,24 @@
 'use strict';
 
 var Queue = function () {
-    
+    this.queue = [];
 };
 
-Queue.prototype.enqueue = function(item){}
+Queue.prototype.enqueue = function(item){
+    this.queue.push(item);
+}
 
-Queue.prototype.dequeue = function(){}
+Queue.prototype.dequeue = function(){
+    this.queue.shift();
+}
 
-Queue.prototype.getQueueFrontItem = function(){}
+Queue.prototype.getQueueFrontItem = function(){
+    this.queue[0];
+}
 
-Queue.prototype.getQueueBackItem = function(){}
+Queue.prototype.getQueueBackItem = function(){
+    this.queue[queue.length - 1];
+}
 
 
 module.exports = Queue;
